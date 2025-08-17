1b44350451537a32842788693e15716cecfa50e5
Lucas Gabriel Witchemichen <lucas.witchemichen@escola.pr.gov.br>
2025-08-17 20:49:51 -0300

    Update flashcard.tsx

--- src/components/flashcards/flashcard.tsx
+++ src/components/flashcards/flashcard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { motion } from "framer-motion";
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
