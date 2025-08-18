48bc871d483fb96054cee51809b8d52dd8188b2b
Lucas Gabriel Witchemichen <lucas.witchemichen@escola.pr.gov.br>
2025-08-18 13:02:02 -0300

    Update command.tsx

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -23,12 +23,46 @@ const Command = React.forwardRef<
 ))
 Command.displayName = CommandPrimitive.displayName
 
-interface CommandDialogProps extends DialogProps {}
+interface CommandDialogProps extends DialogProps {
+  /**
+   * Optional custom class name for the command dialog
+   */
+  containerClassName?: string;
+  /**
+   * Optional custom class name for the command input
+   */
+  inputClassName?: string;
+  /**
+   * Optional custom class name for the command list
+   */
+  listClassName?: string;
+  /**
+   * Placeholder text for the search input
+   */
+  placeholder?: string;
+  /**
+   * Whether to show the search icon
+   */
+  showSearchIcon?: boolean;
+  /**
+   * Callback when the search value changes
+   */
+  onSearchValueChange?: (value: string) => void;
+}
 
-const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
+const CommandDialog = ({ 
+  children, 
+  containerClassName,
+  inputClassName,
+  listClassName,
+  placeholder = "Search...",
+  showSearchIcon = true,
+  onSearchValueChange,
+  ...props 
+}: CommandDialogProps) => {
   return (
     <Dialog {...props}>
-      <DialogContent className="overflow-hidden p-0 shadow-lg">
+      <DialogContent className={cn("overflow-hidden p-0 shadow-lg", containerClassName)}>
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
@@ -39,16 +73,20 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
 
 const CommandInput = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Input>,
-  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>
->(({ className, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input> & {
+    placeholder?: string;
+    showSearchIcon?: boolean;
+  }
+>(({ className, placeholder = "Search...", showSearchIcon = true, ...props }, ref) => (
   <div className="flex items-center border-b px-3" cmdk-input-wrapper="">
-    <Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />
+    {showSearchIcon && <Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />}
     <CommandPrimitive.Input
       ref={ref}
       className={cn(
         "flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
+      placeholder={placeholder}
       {...props}
     />
   </div>
