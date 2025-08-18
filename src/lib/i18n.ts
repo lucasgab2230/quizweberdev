70916f15e32f57f8e8c50d87f780d66aec7cd945
Lucas Gabriel Witchemichen <lucas.witchemichen@escola.pr.gov.br>
2025-08-18 13:09:16 -0300

    Update i18n.ts

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -2,11 +2,12 @@ import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
 
 // Can be imported from a shared config
-const locales = ["en", "pt-BR", "es", "zh", "ja"];
+const locales = ["en", "pt-BR", "es", "zh", "ja"] as const;
+type Locale = typeof locales[number];
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
+  if (!locales.includes(locale as Locale)) notFound();
 
   return {
     messages: (await import(`../../public/locales/${locale}.json`)).default,
